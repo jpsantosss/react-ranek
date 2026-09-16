551bf254cabe088693bc312e9b759f758aabff99
João Pedro Santos <jpedrosantosoliv@gmail.com>
2026-09-16 19:00:18 -0300

    style: adiciona estilos globais

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Contato from "./pages/Contato";
 import Produto from "./pages/Produto";
+import Header from "./layout/Header";
+import "./css/global/Global.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="contato" element={<Contato />} />
-        <Route path="produto" element={<Produto />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="container">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="contato" element={<Contato />} />
+          <Route path="produto" element={<Produto />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
