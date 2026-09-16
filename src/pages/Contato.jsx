3f2ecb7db927a3c47fafa4c9478b9d515c4f1d72
João Pedro Santos <jpedrosantosoliv@gmail.com>
2026-09-16 20:42:17 -0300

    feat: cria página de contato

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,5 +1,24 @@
+import contatoImage from "../assets/img/contato.jpg";
+import "../css/contato/Contato.css";
+
 const Contato = () => {
-  return <div>Página contato</div>;
+  return (
+    <div className="contato-container">
+      <img src={contatoImage} alt="Máquina de escrever" className="contato-image" />
+      <section>
+        <h1 className="contato-title">Entre em contato.</h1>
+        <ul className="contato-list">
+          <li className="contato-item">
+            <a href="mailto:jpedrosantosoliv@gmail.com">jpedrosantosoliv@gmail.com</a>
+          </li>
+          <li className="contato-item">
+            <a href="http://github.com/jpsantosss" target="_blank">github.com/jpsantosss</a>
+          </li>
+          <li className="contato-item">Rio de Janeiro, Brasil</li>
+        </ul>
+      </section>
+    </div>
+  );
 };
 
 export default Contato;
