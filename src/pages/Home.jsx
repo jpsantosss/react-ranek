92d010fef7dceaef120788993558a23deb3171c0
João Pedro Santos <jpedrosantosoliv@gmail.com>
2026-09-16 21:46:37 -0300

    feat: cria página home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,28 @@
+import React from "react";
+import "../css/home/Home.css"
+
 const Home = () => {
-  return <div>Página home</div>;
+  const [produtos, setProdutos] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://ranekapi.origamid.dev/json/api/produto")
+      .then((response) => response.json())
+      .then((json) => setProdutos(json));
+  }, []);
+
+
+  console.log(produtos);
+
+  return (
+    <section className="home-container">
+      {produtos.map((produto) => (
+        <div key={produto.id}>
+          <img src={produto.fotos[0].src} alt={produto.descricao} className="produto-foto" />
+          <h1 className="produto-title">{produto.nome}</h1>
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default Home;
