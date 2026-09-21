19cc3e695d5f354d1acf8a3c195aa3500bb77283
João Pedro Santos <jpedrosantosoliv@gmail.com>
2026-09-21 20:15:09 -0300

    WIP: refatoração para utilização de useFetch

--- src/pages/Produto.jsx
+++ src/pages/Produto.jsx
@@ -1,32 +1,38 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import "../css/produto/Produto.css";
+import useFetch from "../hooks/useFetch";
 
 const Produto = () => {
   const params = useParams();
-  const [produto, setProduto] = React.useState([]);
+  const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
-    fetch(`https://ranekapi.origamid.dev/json/api/produto/${params.id}`)
-      .then((response) => response.json())
-      .then((json) => setProduto(json));
-  }, [params]);
+    request(`https://ranekapi.origamid.dev/json/api/produto/${params.id}`);
+  }, [request, params]);
 
-  return (
+  // React.useEffect(() => {
+  //   fetch(`https://ranekapi.origamid.dev/json/api/produto/${params.id}`)
+  //     .then((response) => response.json())
+  //     .then((json) => setProduto(json));
+  // }, [params]);
+  if (error) return <p>{error}</p>;
+  if (loading) return <p>Carregando...</p>;
+  if (data) return (
     <div className="produto-container">
-      {produto.fotos ? (
+      {data.fotos ? (
         <>
           <section className="image-section">
-            {produto.fotos.map((foto, index) => (
+            {data.fotos.map((foto, index) => (
               <div key={index} className="image-item">
                 <img src={foto.src} alt={foto.titulo} />
               </div>
             ))}
           </section>
           <section>
-            <h1 className="produto-title">{produto.nome}</h1>
-            <span className="produto-price">R$ {produto.preco}</span>
-            <p className="produto-description">{produto.descricao}</p>
+            <h1 className="produto-title">{data.nome}</h1>
+            <span className="produto-price">R$ {data.preco}</span>
+            <p className="produto-description">{data.descricao}</p>
           </section>
         </>
       ) : (
