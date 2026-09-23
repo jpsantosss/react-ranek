444ef97593b2514745a9223aa5a6e493d032b189
João Pedro Santos <jpedrosantosoliv@gmail.com>
2026-09-23 20:16:51 -0300

    feat: novo footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,23 @@ import Contato from "./pages/Contato";
 import Produto from "./pages/Produto";
 import Header from "./layout/Header";
 import "./css/global/Global.css";
+import Footer from "./layout/Footer";
 
 function App() {
   return (
-    <div className="container">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="contato" element={<Contato />} />
-          <Route path="produto/:id" element={<Produto />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      <div className="container">
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="contato" element={<Contato />} />
+            <Route path="produto/:id" element={<Produto />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import "../css/layout/Footer.css"
+
+const Footer = () => {
+  return (
+    <div className="footer-content">
+      <p>Criado por <a href="https://github.com/jpsantosss" target="_blank" className="">João Pedro</a> - Origamid</p>
+    </div>
+  );
+};
+
+export default Footer;
